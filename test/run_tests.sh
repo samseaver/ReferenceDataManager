7facf8957a997e34806a253d84bbcb02ed089c01
Qizhi Zhang <qzhang@hepwl159.hep.anl.gov>
2016-09-20 13:44:02 -0500

    changed the option for the run_test.sh first line

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-script_dir=$(dirname "$(readlink -f "$0")")
+script_dir=$(dirname "$(readlink -n "$0")")
 export KB_DEPLOYMENT_CONFIG=$script_dir/../deploy.cfg
 export KB_AUTH_TOKEN=`cat /kb/module/work/token`
 export PERL5LIB=$script_dir/../lib:$PATH:$PERL5LIB
