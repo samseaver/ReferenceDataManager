4d3c21e4b30f0cad7b504e022a9266863edb3872
Qizhi Zhang <qzzhang@users.noreply.github.com>
2016-09-27 15:50:36 -0500

    Update ReferenceDataManagerImpl.pm

--- lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
+++ lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
@@ -236,6 +236,7 @@ sub _parseResponse
 #Internal Method: to list the genomes already in SOLR and return an array of those genomes
 #
 sub _list_genomes_in_solr {
+	my ($self) = @_;
 	#my $solrServer = "http://kbase.us/internal/solr-ci/search";#$ENV{KBASE_SOLR};
 	my $solrFormat="&wt=json";#"&wt=csv&csv.separator=%09&csv.mv.separator=;";
 	my $core = "/genomes";
