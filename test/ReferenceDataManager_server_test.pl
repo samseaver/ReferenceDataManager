14c6bc9411e5b010770f2d61712be1b5079a3857
Qizhi Zhang <qzzhang@users.noreply.github.com>
2016-09-27 18:42:56 -0500

    Update ReferenceDataManager_server_test.pl

--- test/ReferenceDataManager_server_test.pl
+++ test/ReferenceDataManager_server_test.pl
@@ -21,9 +21,9 @@ my $impl = new ReferenceDataManager::ReferenceDataManagerImpl();
 
 eval {
     my $solr_ret = $impl->_list_genomes_in_solr();#{response}->{response}->{docs}
-    print "Example solr records value: ".$solr_ret->{response}->{grouped}->{genome_id}->{groups}->[0]->{groupValue}."\n";
-    print "Example solr records count: ".$solr_ret->{response}->{grouped}->{genome_id}->{groups}->[0]->{doclist}->{numFound}."\n";
-    print Dumper($solr_ret->{grouped}->{groups});
+    print "\nExample solr records value: ".$solr_ret->{response}->{grouped}->{genome_id}->{groups}->[0]->{groupValue}."\n";
+    print "\nExample solr records count: ".$solr_ret->{response}->{grouped}->{genome_id}->{groups}->[0]->{doclist}->{numFound}."\n";
+    print Dumper($solr_ret->{grouped}->{genome_id}->{groups});
     #Altering workspace map
     $impl->{_workspace_map}->{refseq} = "RefSeqTest";
     #Testing the list_reference_genomes function
