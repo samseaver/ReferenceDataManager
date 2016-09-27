ba81f55ae6d72a81728d6e390da028d4290565e4
Qizhi Zhang <qzzhang@users.noreply.github.com>
2016-09-27 14:02:46 -0500

    Update ReferenceDataManagerImpl.pm

--- lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
+++ lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
@@ -204,7 +204,7 @@ sub _list_genomes_in_solr {
 	my $solr_json_response = JSON::from_json($solr_response->{response});
 	print "\nRaw response: \n". $solr_response;
 	print "\nJSON response: \n" . $solr_json_response;
-	my @genome_records = @{$solr_json_response->{docs}};
+	my @genome_records = $solr_json_response->{docs};
 	#my $records_total = $solr_json_response->{numFound};
 	return @genome_records;
 }
