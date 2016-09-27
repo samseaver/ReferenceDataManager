82a9e49275bd8f8d4237e6aeed423deadfad8b4f
Qizhi Zhang <qzzhang@users.noreply.github.com>
2016-09-27 17:16:02 -0500

    Update ReferenceDataManagerImpl.pm

--- lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
+++ lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
@@ -249,7 +249,7 @@ sub _list_genomes_in_solr {
 	print "\n$solrQuery\n";
 	#my $solr_response =`curl "$solrQuery"`; #`wget -q -O - "$solrQuery" | grep -v genome_name`;
 	my $solr_response = $self->_request("$solrQuery", "GET");
-	print "\nRaw response: \n" . $solr_response->{response} . "\n";
+	#print "\nRaw response: \n" . $solr_response->{response} . "\n";
 	my $responseCode = $self->_parseResponse($solr_response, $resultformat);
     	if ($responseCode) {
         	if ($resultformat eq "json") {
