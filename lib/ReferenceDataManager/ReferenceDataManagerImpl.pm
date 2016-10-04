01de1e9cd8ad3288cd66a5dac2cac2a861194485
Qizhi Zhang <qzzhang@users.noreply.github.com>
2016-10-04 13:36:15 -0500

    Update ReferenceDataManagerImpl.pm

--- lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
+++ lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
@@ -1432,104 +1432,104 @@ sub index_genomes_in_solr
 
     foreach my $kbase_genome_data (@$params)
     {
-	my $record;
-	my $ws_name = $kbase_genome_data->{workspace_name};
-	my $ws_genome_name = $kbase_genome_data->{name}; 
-	my $genome_source = $kbase_genome_data->{source};
-	my $ws_genome_metadata  = `ws-get -w $ws_name $ws_genome_name -m`;
-	my @genome_metadata = split(/\n/, $ws_genome_metadata);
-
-	foreach my $metadata (@genome_metadata){
-	  my ($ws_genome_id) = $metadata=~/Object ID:(\d+)/ if $metadata=~/Object ID:(\d+)/;
-	}
-
-	$record->{workspace_name} = $ws_name; # KBasePublicRichGenomesV5
-	$record->{object_id} = $ws_genome_name; #"kb|ws.".$ws_id.".obj."."$ws_genome_id"; # kb|ws.2869.obj.9837
-	$record->{object_name} = $ws_genome_name; # kb|g.3397
-	$record->{object_type} = "KBaseGenomes.Genome-8.0"; # KBaseSearch.Genome-5.0 / KBaseGenomes.Genome-8.0
-
-	# Get genome info
-	my $ws_genome  = $json->decode(`ws-get -w $ws_name $ws_genome_name`);
-	$record->{genome_id} = $ws_genome_name; #$ws_genome->{id}; # kb|g.3397
-	$record->{genome_source} = $genome_source; # $ws_genome->{external_source}; # KBase Central Store
-	$record->{genome_source_id} = $ws_genome->{external_source_id}; # 83332.12
-	$record->{num_cds} = $ws_genome->{counts_map}->{CDS};
-
-	# Get assembly info
-	my $ws_assembly = $json->decode(`ws-get $ws_genome->{assembly_ref}`);
-	$record->{genome_dna_size} = $ws_assembly->{dna_size};
-	$record->{num_contigs} = $ws_assembly->{num_contigs};
-	$record->{complete} = ""; #$ws_genome->{complete}; # type?? 
-	$record->{gc_content} = $ws_genome->{gc_content};
-
-	# Get taxon info
-	my $ws_taxon = $json->decode(`ws-get $ws_genome->{taxon_ref}`);
-	$record->{scientific_name} = $ws_taxon->{scientific_name};
-	$record->{taxonomy} = $ws_taxon->{scientific_lineage};
-	$record->{taxonomy} =~s/ *; */;/g;
-	#$record->{tax_id} = $ws_taxon->{taxonomy_id};
-	$record->{domain} = $ws_taxon->{domain};
-
-	#$genome->{genome_publications}=$ws_genome->{};
-	#$genome->{has_publications}=$ws_genome->{};
-
-	push (@{solr_records}, $record);
-
-	#print Dumper(\@{solr_records});
-
-	# Prepare feature records for solr
-
-	foreach my $feature_type (keys %{$ws_genome->{feature_container_references}})
-	{
- 	    my $container_ref = $ws_genome->{feature_container_references}->{$feature_type};
- 	    my $ws_features = $json->decode(`ws-get $container_ref`);
-
- 	    #print Dumper ($ws_features);
-
- 	    foreach my $key (keys %{$ws_features->{features}})
-	    {
-		my $feature = $ws_features->{features}->{$key};
 		my $record;
+		my $ws_name = $kbase_genome_data->{workspace_name};
+		my $ws_genome_name = $kbase_genome_data->{name}; 
+		my $genome_source = $kbase_genome_data->{source};
+		my $ws_genome_metadata  = `ws-get -w $ws_name $ws_genome_name -m`;
+		my @genome_metadata = split(/\n/, $ws_genome_metadata);
 
+		foreach my $metadata (@genome_metadata){
+	  		my ($ws_genome_id) = $metadata=~/Object ID:(\d+)/ if $metadata=~/Object ID:(\d+)/;
+		}
+		
 		$record->{workspace_name} = $ws_name; # KBasePublicRichGenomesV5
-		$record->{object_id} = $feature->{feature_id}; # kb|ws.2869.obj.9836/features/kb|g.3397.peg.3821
-		$record->{object_name} = $feature->{feature_id}; # kb|g.3397.featureset/features/kb|g.3397.peg.3821
-		$record->{object_type} = "KBaseSearch.Feature"; # KBaseSearch.Feature
+		$record->{object_id} = $ws_genome_name; #"kb|ws.".$ws_id.".obj."."$ws_genome_id"; # kb|ws.2869.obj.9837
+		$record->{object_name} = $ws_genome_name; # kb|g.3397
+		$record->{object_type} = "KBaseGenomes.Genome-8.0"; # KBaseSearch.Genome-5.0 / KBaseGenomes.Genome-8.0
 
+		# Get genome info
+		my $ws_genome  = $json->decode(`ws-get -w $ws_name $ws_genome_name`);
 		$record->{genome_id} = $ws_genome_name; #$ws_genome->{id}; # kb|g.3397
 		$record->{genome_source} = $genome_source; # $ws_genome->{external_source}; # KBase Central Store
 		$record->{genome_source_id} = $ws_genome->{external_source_id}; # 83332.12
+		$record->{num_cds} = $ws_genome->{counts_map}->{CDS};
 
+		# Get assembly info
+		my $ws_assembly = $json->decode(`ws-get $ws_genome->{assembly_ref}`);
+		$record->{genome_dna_size} = $ws_assembly->{dna_size};
+		$record->{num_contigs} = $ws_assembly->{num_contigs};
+		$record->{complete} = ""; #$ws_genome->{complete}; # type?? 
+		$record->{gc_content} = $ws_genome->{gc_content};
+
+		# Get taxon info
+		my $ws_taxon = $json->decode(`ws-get $ws_genome->{taxon_ref}`);
 		$record->{scientific_name} = $ws_taxon->{scientific_name};
 		$record->{taxonomy} = $ws_taxon->{scientific_lineage};
 		$record->{taxonomy} =~s/ *; */;/g;
 		#$record->{tax_id} = $ws_taxon->{taxonomy_id};
 		$record->{domain} = $ws_taxon->{domain};
 
-		$record->{feature_type} = $feature->{type}; 
-		$record->{feature_id} = $feature->{feature_id}; 
-		$record->{feature_source_id} = $feature->{feature_id}; 
-		$record->{function} = $feature->{function}; 
-	
-		# aliases
-		my @aliases;
-		foreach my $key (keys %{$feature->{aliases}}){
-			push @aliases, "$feature->{aliases}->{$key}[0]:$key";
-			$record->{gene_name} = $key if $feature->{aliases}->{$key}[0]=~/Genbank Gene/i;	 
-		}
-		$record->{aliases} = join(" :: ", @aliases);
-	
-		my $last_location = scalar @{$feature->{locations}};
-		$record->{location_contig} = $feature->{locations}[0][0]; 
-		$record->{location_begin} = $feature->{locations}[0][1]; 
-		$record->{location_end} = $feature->{locations}[$last_location][1]+$feature->{location}[$last_location][3]; 
-		$record->{location_strand} = $feature->{locations}[0][2]; 
-		$record->{locations} = $json->pretty->encode($feature->{locations});
-		$record->{locations} =~s/\s*//g; 
-		$record->{locations} =~s/,\[\]//g; 
+		#$genome->{genome_publications}=$ws_genome->{};
+		#$genome->{has_publications}=$ws_genome->{};
 
-		$record->{protein_translation_length} = $feature->{protein_translation_length}; 
-		$record->{dna_sequence_length} = $feature->{dna_sequence_length}; 
+		push (@{solr_records}, $record);
+
+		#print Dumper(\@{solr_records});
+
+		# Prepare feature records for solr
+
+		foreach my $feature_type (keys %{$ws_genome->{feature_container_references}})
+		{
+ 	    	my $container_ref = $ws_genome->{feature_container_references}->{$feature_type};
+ 	    	my $ws_features = $json->decode(`ws-get $container_ref`);
+
+ 	    	#print Dumper ($ws_features);
+
+ 	    	foreach my $key (keys %{$ws_features->{features}})
+	    	{
+				my $feature = $ws_features->{features}->{$key};
+				my $record;
+
+				$record->{workspace_name} = $ws_name; # KBasePublicRichGenomesV5
+				$record->{object_id} = $feature->{feature_id}; # kb|ws.2869.obj.9836/features/kb|g.3397.peg.3821
+				$record->{object_name} = $feature->{feature_id}; # kb|g.3397.featureset/features/kb|g.3397.peg.3821
+				$record->{object_type} = "KBaseSearch.Feature"; # KBaseSearch.Feature
+
+				$record->{genome_id} = $ws_genome_name; #$ws_genome->{id}; # kb|g.3397
+				$record->{genome_source} = $genome_source; # $ws_genome->{external_source}; # KBase Central Store
+				$record->{genome_source_id} = $ws_genome->{external_source_id}; # 83332.12
+
+				$record->{scientific_name} = $ws_taxon->{scientific_name};
+				$record->{taxonomy} = $ws_taxon->{scientific_lineage};
+				$record->{taxonomy} =~s/ *; */;/g;
+				#$record->{tax_id} = $ws_taxon->{taxonomy_id};
+				$record->{domain} = $ws_taxon->{domain};
+
+				$record->{feature_type} = $feature->{type}; 
+				$record->{feature_id} = $feature->{feature_id}; 
+				$record->{feature_source_id} = $feature->{feature_id}; 
+				$record->{function} = $feature->{function}; 
+	
+				# aliases
+				my @aliases;
+				foreach my $key (keys %{$feature->{aliases}}){
+					push @aliases, "$feature->{aliases}->{$key}[0]:$key";
+					$record->{gene_name} = $key if $feature->{aliases}->{$key}[0]=~/Genbank Gene/i;	 
+				}
+				$record->{aliases} = join(" :: ", @aliases);
+	
+				my $last_location = scalar @{$feature->{locations}};
+				$record->{location_contig} = $feature->{locations}[0][0]; 
+				$record->{location_begin} = $feature->{locations}[0][1]; 
+				$record->{location_end} = $feature->{locations}[$last_location][1]+$feature->{location}[$last_location][3]; 
+				$record->{location_strand} = $feature->{locations}[0][2]; 
+				$record->{locations} = $json->pretty->encode($feature->{locations});
+				$record->{locations} =~s/\s*//g; 
+				$record->{locations} =~s/,\[\]//g; 
+
+				$record->{protein_translation_length} = $feature->{protein_translation_length}; 
+				$record->{dna_sequence_length} = $feature->{dna_sequence_length}; 
 
 =for comment	
 	$record->{roles} = $feature->{}; 
@@ -1545,21 +1545,21 @@ sub index_genomes_in_solr
 	$record->{feature_publications} = $feature->{}; 
 =cut
 
-		push @solr_records, $record;
-	    }
-	}
-	#print Dumper (\@solr_records);
+				push @solr_records, $record;
+	    	}
+		}
+		#print Dumper (\@solr_records);
 
-	my $genome_json = $json->pretty->encode(\@solr_records);
+		my $genome_json = $json->pretty->encode(\@solr_records);
 
-	my $genome_file = $self->{scratch}."$ws_genome_name.json";
+		my $genome_file = $self->{scratch}."$ws_genome_name.json";
 
-	open FH, ">$genome_file" or die "Cannot write to genome.json: $!";
-	print FH "$genome_json";
+	#open FH, ">$genome_file" or die "Cannot write to genome.json: $!";
+	#print FH "$genome_json";
 	close FH;
 
-	`$Bin/post_solr_update.sh genomes $genome_file`;#By default we assume all to be indexed--if $opt->index=~/y|yes|true|1/i;
-	push (@{$output}, $kbase_genome_data);
+	#`$Bin/post_solr_update.sh genomes $genome_file`;#By default we assume all to be indexed--if $opt->index=~/y|yes|true|1/i;
+		push (@{$output}, $kbase_genome_data);
     }
         
     if ($params->{create_report}) {
