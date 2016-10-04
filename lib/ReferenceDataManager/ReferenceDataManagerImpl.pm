74e6b95077975256a8725964423e705a29dbf9e1
Qizhi Zhang <qzzhang@users.noreply.github.com>
2016-10-04 10:24:50 -0500

    Update ReferenceDataManagerImpl.pm

--- lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
+++ lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
@@ -247,7 +247,7 @@ sub _testActionsInSolr
 #
 sub _listGenomesInSolr {
 	my ($self, $solrCore, $fields, $grp) = @_;
-	my $count = 100;#2,147,483,647 is integer's maximum value
+	my $count = 20;#2,147,483,647 is integer's maximum value
 	my $start = 0;
 	my $rows = "&rows=100";
   	my $sort = "&sort=genome_id asc";
