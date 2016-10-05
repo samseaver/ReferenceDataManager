b67a5432903cd8cdd491ecadd7396694561773f1
Qizhi Zhang <qzzhang@users.noreply.github.com>
2016-10-05 10:59:58 -0500

    Update ReferenceDataManager_server_test.pl

--- test/ReferenceDataManager_server_test.pl
+++ test/ReferenceDataManager_server_test.pl
@@ -11,7 +11,7 @@ local $| = 1;
 my $token = $ENV{'KB_AUTH_TOKEN'};
 my $config_file = $ENV{'KB_DEPLOYMENT_CONFIG'};
 my $config = new Config::Simple($config_file)->get_block('ReferenceDataManager');
-#my $ws_url = $config->{"workspace-url"};
+my $ws_url = $config->{"workspace-url"};
 #my $ws_name = undef;
 my $ws_client = new Bio::KBase::workspace::Client($ws_url,token => $token);
 my $auth_token = Bio::KBase::AuthToken->new(token => $token, ignore_authrc => 1);
