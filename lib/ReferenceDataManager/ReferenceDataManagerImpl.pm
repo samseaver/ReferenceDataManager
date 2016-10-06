26d7f6962d10c42c29fa80f7efefd7374fef34a2
Qizhi Zhang <qzzhang@users.noreply.github.com>
2016-10-06 12:01:02 -0500

    Update ReferenceDataManagerImpl.pm

--- lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
+++ lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
@@ -1474,8 +1474,9 @@ sub index_genomes_in_solr
 		my $ws_name = $kbase_genome_data->{workspace_name};
 		my $ws_genome_name = $kbase_genome_data->{id}; 
 		my $genome_source = $kbase_genome_data->{source};
+		my $ws_genome_metadata = {};
 		if(defined($self->util_ws_client())){
-    		my $ws_genome_metadata = $self->util_ws_client()->get_objects({
+    		$ws_genome_metadata = $self->util_ws_client()->get_objects({
 				object_refs => "$ws_name/$ws_genome_name"});#`ws-get -w $ws_name $ws_genome_name -m`;	
 			print "ws_genome_metadata: \n" . Dumper($ws_genome_metadata) . "\nwith workspace_name$ws_name";
 		}
