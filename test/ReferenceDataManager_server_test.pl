8332c9f710eb339746b4a325e6091b2b21a80b4b
Qizhi Zhang <qzzhang@users.noreply.github.com>
2016-10-07 17:59:05 -0500

    Update ReferenceDataManager_server_test.pl

--- test/ReferenceDataManager_server_test.pl
+++ test/ReferenceDataManager_server_test.pl
@@ -29,7 +29,8 @@ my $impl = new ReferenceDataManager::ReferenceDataManagerImpl();
 
 eval {
     #$impl->_testActionsInSolr();
-    $impl->_testLoadGenomes();
+	$impl->_testListGenomes();
+    #$impl->_testLoadGenomes();
     exit 0;#to not go further
 	
     #Altering workspace map
