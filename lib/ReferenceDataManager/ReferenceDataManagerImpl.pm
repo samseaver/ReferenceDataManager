7ecc47dd4f000f060341683839155452f883571b
Qizhi Zhang <qzzhang@users.noreply.github.com>
2016-10-10 15:39:08 -0500

    Update ReferenceDataManagerImpl.pm

--- lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
+++ lib/ReferenceDataManager/ReferenceDataManagerImpl.pm
@@ -1788,9 +1788,9 @@ sub update_loaded_genomes
     $output = [];
 	print "\nTesting within update_loaded_genomes\n";
     my $count = 0;
-    my $ref_genomes = $self->list_reference_genomes({refseq => $params->{refseq}, update_only => $params->{update_only});
-    my $loaded_genomes = $self->list_loaded_genomes({refseq => $params->{refseq});
-    my @genomes_in_solr = ($self->_listGenomesInSolr("QZtest", "*"))->{response}->{response}->{docs};    
+    my $ref_genomes = $self->list_reference_genomes({refseq => $params->{refseq}, update_only => $params->{update_only}});
+    my $loaded_genomes = $self->list_loaded_genomes({refseq => $params->{refseq}});
+    my @genomes_in_solr = $self->_listGenomesInSolr("QZtest", "*")->{response}->{response}->{docs};    
 
     for (my $i=0; $i <@{ $ref_genomes }; $i++) {
 		my $genome = $ref_genomes->[$i];
