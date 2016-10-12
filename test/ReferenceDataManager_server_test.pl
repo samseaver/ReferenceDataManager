a9da0175da19a0756bc8d69d950f2bbc043072bf
Qizhi Zhang <qzzhang@users.noreply.github.com>
2016-10-12 13:56:23 -0500

    Update ReferenceDataManager_server_test.pl

--- test/ReferenceDataManager_server_test.pl
+++ test/ReferenceDataManager_server_test.pl
@@ -30,8 +30,8 @@ my $impl = new ReferenceDataManager::ReferenceDataManagerImpl();
 eval {
     #$impl->_testActionsInSolr_passed();
 	#$impl->_testListGenomes();
-    #$impl->_testLoadGenomes();
-    #exit 0;#to not go further
+    $impl->_testLoadGenomes();
+    exit 0;#to not go further
 	
     #Altering workspace map
     #$impl->{_workspace_map}->{refseq} = "RefSeqTest";
